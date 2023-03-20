74f4a42fe19b5463cfa29337420c68932957ba5c
Caren Salles <carencrisbsalles@gmail.com>
2023-03-21 00:32:12 +0100

    puting dependencies lines

--- src/main/frontend/vite.config.js
+++ src/main/frontend/vite.config.js
@@ -8,7 +8,14 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    test: {
+      globals: true,
+      enviroment: "jsdom",
+    },
+    build: {
+      outDir: "docs"
+    },
+  },
+});
