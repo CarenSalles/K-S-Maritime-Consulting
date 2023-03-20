4ecce3e0e9c15f0ca3a20aa5f7d60522a0ca4a00
Caren Salles <carencrisbsalles@gmail.com>
2023-03-20 23:27:10 +0100

    first commit

---
+++ src/main/java/com/caren/ksmaritimeconsulting/KsMaritimeConsultingApplication.java
@@ -0,0 +1,13 @@
+package com.caren.ksmaritimeconsulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsMaritimeConsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsMaritimeConsultingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caren/ksmaritimeconsulting/KsMaritimeConsultingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caren.ksmaritimeconsulting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsMaritimeConsultingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
