1d19b19da29d77443004e094e62037bfedb59d25
Caren Salles <carencrisbsalles@gmail.com>
2023-04-11 18:43:50 +0200

    Navbar Component frontend started

--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import NavbarComponent from '../components/NavbarComponent.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <NavbarComponent />
   </main>
 </template>
---
+++ src/main/frontend/src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="login">
+
+  </div>
+</template>
+
+<style>
+
+</style>
